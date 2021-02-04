fd9133d6765b28144b5a6bc04fe95f72a9f02d6e
Lladruc37 <sergi@kromatika.com>
2021-02-04 13:22:57 +0100

    started working on vines

--- MissingChord/Assets/Scripts/PlayerController.cs
+++ MissingChord/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpHeight;
     bool jump = false;
     bool jumpAvailable = true;
+    public bool climbAvailable = false;
+    bool isClimbing = false;
     Vector2 movement;
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,21 @@ public class PlayerController : MonoBehaviour
         movement = new Vector2(0.0f, 0.0f);
         movement.x = Input.GetAxisRaw("Horizontal");
 
+        if(climbAvailable)
+        {
+            movement.y = Input.GetAxisRaw("Vertical");
+            if(movement.y != 0.0f)
+            {
+                isClimbing = true;
+                rb.gravityScale = 0.0f;
+                //Physics2D.gravity = new Vector2(0.0f, 0.0f);
+            }
+        }
+        else
+        {
+            rb.gravityScale = 5.0f;
+        }
+
         if (Input.GetButtonDown("Jump") && jumpAvailable)
         {
             jump = true;
@@ -32,31 +49,30 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.position += new Vector3(movement.x * Time.fixedDeltaTime * speed, 0.0f, 0.0f);
-
-        //bool wasGrounded = isGrounded;
-        //jump = false;
-
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundSensor.position, 0.1f, playerMask);
-        for (int i = 0; i < colliders.Length; i++)
+        if (!isClimbing)
         {
-            if (colliders[i].gameObject != gameObject)
+            transform.position += new Vector3(movement.x * Time.fixedDeltaTime * speed, 0.0f, 0.0f);
+
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(groundSensor.position, 0.1f, playerMask);
+            for (int i = 0; i < colliders.Length; i++)
             {
-                jumpAvailable = true;
-                Debug.Log("Collision!");
-                //if (wasGrounded)
-                //{
-                //    rb.AddForce(new Vector2(0.0f, jumpHeight));
-                //    //jump = false;
-                //}
-            }
-            else
-            {
-                jumpAvailable = false;
+                if (colliders[i].gameObject != gameObject)
+                {
+                    jumpAvailable = true;
+                }
+                else
+                {
+                    jumpAvailable = false;
+                }
             }
         }
+        else
+        {
+            jumpAvailable = true;
+            transform.position += new Vector3(movement.x * Time.fixedDeltaTime * speed, movement.y * Time.fixedDeltaTime * speed, 0.0f);
+        }
 
-        if(jump)
+        if (jump)
         {
             jumpAvailable = false;
             jump = false;
---
+++ MissingChord/Assets/StairsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsController : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.climbAvailable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.climbAvailable = false;
+    }
+}
